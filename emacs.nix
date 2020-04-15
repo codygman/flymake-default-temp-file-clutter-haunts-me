15bb7bd220592f2c32e56c1de43c1925a8184612
codygman <cody@codygman.dev>
2020-04-15 01:03:39 -0500

    try creating own flymake pkg from source

--- emacs.nix
+++ emacs.nix
@@ -2,22 +2,20 @@
 let
   myEmacs = pkgs.emacs;
   overrides = self: super: {
-    flymake = super.flymake.overrideAttrs (oa: {
-      src = builtins.fetchGit {
+    flymake = pkgs.emacsMelpa.callPackage (
+      builtins.fetchGit {
         url = "git@github.com:flymake/emacs-flymake.git";
         rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
         ref = "master";
-      };
-    });
-  };
-  emacsWithPackages = ((pkgs.emacsPackagesGen myEmacs).overrideScope' overrides).emacsWithPackages (p: with p; [
-    p.haskellMode
-    p.flymake
-  ]);
+      });
+    };
 in
 (pkgs.stdenv.mkDerivation {
   name = "debug-flymake-annoying-clutter";
   buildInputs = [
-    emacsWithPackages
+    (((pkgs.emacsPackagesGen myEmacs).overrideScope' overrides).emacsWithPackages (p: with p; [
+      p.haskellMode
+      p.flymake
+    ]))
   ];
 })
