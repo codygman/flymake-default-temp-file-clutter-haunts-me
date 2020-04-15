00723a08e7f500d6e8b8f453dc7974637f981878
codygman <cody@codygman.dev>
2020-04-15 01:04:05 -0500

    try giving arg to melpaBuild

--- emacs.nix
+++ emacs.nix
@@ -7,7 +7,7 @@ let
         url = "git@github.com:flymake/emacs-flymake.git";
         rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
         ref = "master";
-      });
+      }) {};
     };
 in
 (pkgs.stdenv.mkDerivation {
