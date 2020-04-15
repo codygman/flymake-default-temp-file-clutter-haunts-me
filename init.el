64de9c95c82da6939736e9902a2d9b063817c937
codygman <cody@codygman.dev>
2020-04-15 00:23:34 -0500

    cleanup init to make intent clearer

--- init.el
+++ init.el
@@ -1,8 +1,19 @@
-(find-file "foo.hs")
-(require 'haskell-mode)
 ;; Nope, I want my copies in the system temp dir.
 (setq flymake-run-in-place nil)
 ;; This lets me say where my temp dir is.
 (setq temporary-file-directory "~/code/debug/flymake-nix-disable-in-project-tmp/tmp/")
+
+;; open haskell file
+(find-file "foo.hs")
+
+;; start haskell and flymake modes
+(require 'haskell-mode)
+(haskell-mode)
 (flymake-mode)
+
+;; if this is the old version of flymake it'll create a foo_flymake*.hs file
+;; if this is the new version of flymake it'll create a tmp/foo_flymake*.hs file
+
+;; debug output to see if the outputs look correct
 (princ (locate-library "flymake"))
+(princ (locate-library "eglot"))
