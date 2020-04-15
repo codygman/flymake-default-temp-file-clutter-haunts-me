e950d4c6143cc46a203c98c040a7c7a5cbfcb01d
codygman <cody@codygman.dev>
2020-04-15 01:03:55 -0500

    try melpaBuild

--- emacs.nix
+++ emacs.nix
@@ -2,7 +2,7 @@
 let
   myEmacs = pkgs.emacs;
   overrides = self: super: {
-    flymake = pkgs.emacsMelpa.callPackage (
+    flymake = pkgs.emacsMelpa.melpaBuild (
       builtins.fetchGit {
         url = "git@github.com:flymake/emacs-flymake.git";
         rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
