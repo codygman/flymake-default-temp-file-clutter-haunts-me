85c8f26070cd1cb6f8681257bfdebe456e3c06eb
codygman <cody@codygman.dev>
2020-04-15 01:11:14 -0500

    try using melpabuidl example
    https://github.com/cantsin/remacs-nix/blob/a323cc5077bc7082c6c1a06e0c3a485b817b2abd/packages.nix

--- emacs.nix
+++ emacs.nix
@@ -2,12 +2,51 @@
 let
   myEmacs = pkgs.emacs;
   overrides = self: super: {
-    flymake = pkgs.emacsMelpa.melpaBuild (
-      builtins.fetchGit {
-        url = "git@github.com:flymake/emacs-flymake.git";
-        rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
-        ref = "master";
-      }) {};
+    lua-mode = (super.lua-mode.override (args: {
+      melpaBuild = drv: args.melpaBuild (drv // {
+        src = pkgs.fetchFromGitHub {
+          owner = "immerrr";
+          repo = "lua-mode";
+          rev = "95c64bb5634035630e8c59d10d4a1d1003265743";
+          sha256 = "0cawb544qylifkvqads307n0nfqg7lvyphqbpbzr2xvr5iyi4901";
+          # date = 2019-01-13T13:50:39+03:00;
+        };
+      });
+    }));
+
+
+    flymake = (super.flymake.override (args: {
+      melpaBuild = drv: args.melpaBuild (drv);
+    }));
+    # flymake = (super.flymake.override (args: {
+    #   melpaBuild = drv: args.melpaBuild (drv // {
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "flymake";
+    #       repo = "emacs-flymake";
+    #       rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
+    #       # sha256 = "0000000000000000000000000000000000000000000000000000";
+    #     };
+    #   });
+    # }));
+
+
+
+    # flymake = (super.flymake.override (args: {
+    #   melpaBuild = drv: args.melpaBuild (drv // {
+    #     src = builtins.fetchGit {
+    #       url = "git@github.com:flymake/emacs-flymake.git";
+    #       rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
+    #       ref = "master";
+    #     };
+    #   });
+    # }));
+
+    # flymake = pkgs.emacsMelpa.melpaBuild (
+      # builtins.fetchGit {
+      #   url = "git@github.com:flymake/emacs-flymake.git";
+      #   rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
+      #   ref = "master";
+    #   }) {};
     };
 in
 (pkgs.stdenv.mkDerivation {
