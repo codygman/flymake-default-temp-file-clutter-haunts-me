cbc91421d0d94734bad747ec03f0fa5cc952002d
codygman <cody@codygman.dev>
2020-04-15 00:44:15 -0500

    WIP try override propogatedUserEnvPkgs

--- emacs.nix
+++ emacs.nix
@@ -9,10 +9,36 @@ let
         ref = "master";
       };
     });
+    eglot = super.eglot.overrideAttrs (oa: {
+      # this didn't work
+      # flymake = self.flymake;
+      #
+      # I see in the repl:
+      # nix-repl> pkgs.emacsMelpa.eglot.propagatedUserEnvPkgs
+      # [ «derivation /nix/store/ir9kz2p6yihadycc8h4ifibbn8bba5wa-emacs-flymake-1.0.8.drv» «derivation /nix/store/m17asap91x4pax2ay0nykgpk504wsi2x-emacs-jsonrpc-1.0.9.drv» ]
+      # maybe i can override that
+      propogatedUserEnvPkgs = [];
+    });
   };
-  emacsWithPackages = ((pkgs.emacsPackagesGen myEmacs).overrideScope' overrides).emacsWithPackages;
+  emacsWithPackages = ((pkgs.emacsPackagesGen myEmacs).overrideScope' overrides).emacsWithPackages (p: with p; [
+    p.haskellMode
+    p.eglot
+  ]);
 in
-  emacsWithPackages (p: with p; [
-    haskellMode
-    eglot
-  ])
+(pkgs.stdenv.mkDerivation {
+  name = "debug-flymake-annoying-clutter";
+  buildInputs = [
+    emacsWithPackages
+  ];
+})
+# in
+# pkgs.stdenv.mkDerivation {
+#   name = "debug-flymake-annoying-clutter";
+#   buildInputs = [ emacsWithPackages (p: []) ];
+  # buildInputs = [
+  #   emacsWithPackages (p: with p; [
+  #     haskellMode
+  #     eglot
+  #   ])
+  # ];
+# }
