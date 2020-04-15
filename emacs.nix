5ed9bd31fdf44ae8525065054a1ec3308200233e
codygman <cody@codygman.dev>
2020-04-15 01:19:03 -0500

    irc rewrite using elpabuild
    https://logs.nix.samueldr.com/nixos/2020-04-15#3322898;
    
    it shows that we load the right library:
    
    [cody@nixos:~/code/debug/flymake-nix-disable-in-project-tmp]$ ./test-bug.sh
    Loading /nix/store/a3gxy0l63zg7100f8x7r6ai1z06l0js1-emacs-26.3/share/emacs/site-lisp/site-start.el (source)...
    Loading /home/cody/code/debug/flymake-nix-disable-in-project-tmp/init.el (source)...
    value of flymake-run-in-place: nil
    /nix/store/2rl23nljwdmnx30wpjbks7g8cbq6jb4l-emacs-packages-deps/share/emacs/site-lisp/elpa/flymake-0.4.16/flymake.elc
    
    yet for some reason a damn temp file is still created

--- emacs.nix
+++ emacs.nix
@@ -2,13 +2,16 @@
 let
   myEmacs = pkgs.emacs;
   overrides = self: super: {
-    flymake = pkgs.emacsPackages.trivialBuild (args: (
-      builtins.fetchGit {
-        url = "git@github.com:flymake/emacs-flymake.git";
-        rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
-        ref = "master";
-      }));
+    flymake = super.flymake.override {
+      elpaBuild = args: super.elpaBuild (args // {
+        src = (builtins.fetchGit {
+          url = "git@github.com:flymake/emacs-flymake.git";
+          rev = "8701c806d5e8f4e2c6f7dea461273482ad89c029";
+          ref = "master";
+        }) + "/flymake.el";
+      });
     };
+  };
 in
 (pkgs.stdenv.mkDerivation {
   name = "debug-flymake-annoying-clutter";
